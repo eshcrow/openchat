845039e5ec5af9395fd22c81566237d4b2f901e1
dcc0 <ivangavr777@gmail.com>
2019-03-04 12:22:34 +0300

    Update main.php

--- pages/main.php
+++ pages/main.php
@@ -23,11 +23,6 @@ require_once LIBJS.'scroll_to_bottom.js';
 require_once LIBJS.'cross_object.js';
 require_once HTML.'send_form.html';
 require_once LIBJS.'send.js';
-require_once LIB.'delete_messages.php';	
-    
-        /*Удалим лишние сообщения*/
-        
-    
-        
+require_once LIB.'delete_messages.php';	       
 require_once STYLE.'footer.html';
 ?>

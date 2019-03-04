ccd86e6561240c18857c8c565bd9fcb73f61ee7e
dcc0 <ivangavr777@gmail.com>
2019-03-04 11:17:50 +0300

    Update config.php

--- conf/config.php
+++ conf/config.php
@@ -1,15 +1,25 @@
 <?php
-    function connect() {
-$link = mysqli_connect("localhost", "root", "123", "chat");
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-mysqli_set_charset($link, "utf8");
-return $link;
-}
-$link=connect();
+class connect {
+	
+	public $link;
+	protected $host = 'localhost';
+	protected $user = 'u0643076_sneg';
+	protected $password = 'Zfrae4pN+vlF';
+	protected $db_name = 'u0643076_sneg';
+	
+protected function mysqld () {
+	
+	$this->link=mysqli_connect($this->host, $this->user, $this->password, $this->db_name ) or exit('No connection...');
+		 
+	  }
+	  
+	  function mysql () {
+			$this->mysqld();
+			  
+		}
+	}
+$result = new connect();
+$result->mysql();
+$result->link->set_charset("utf8");
 
 ?>

28e5f1618d226b23f6f96a380a2c7eb00905e65c
dcc0 <ivangavr777@gmail.com>
2019-03-05 00:50:23 +0300

    Update send.php

--- lib/send.php
+++ lib/send.php
@@ -4,8 +4,8 @@ require_once $_SERVER['CONTEXT_DOCUMENT_ROOT'].'/conf/config.php';
 	if($_SERVER['REQUEST_URI'] == '/lib/send.php') header("Location: ../index.php");;	
 
         /*Текст и имя из формы*/
-		$gettext=addslashes($_GET['text']);
-		$userlogin=addslashes($_GET['userlogin']);
+		$gettext=$_GET['text'];
+		$userlogin=$_GET['userlogin'];
 		
 		
 class Send_messages {    		
@@ -13,42 +13,53 @@ class Send_messages {
 
     protected $send;
     protected $chat="chat";
-
+    protected $login;
+    protected $text;
     
 	function checkTextInput($link, $gettext, $userlogin) {
+		
+
+		$gettext = substr($gettext, 0,300);
+		$userlogin=substr($userlogin, 0,15);;
 		
 		/*Проверим ввод текста*/
 		if(isset($gettext) && $gettext !='' ) 
 		{	
-			$gettext = substr($gettext, 0,300);
+			
 		
 		if(!preg_match('/^[a-zа-яё0-9\.\,\!\?\;\#\&\/\)\(\-\+\=:\s]+$/ui', $gettext)) 
 		{		
-		$this->send=$link->query("INSERT INTO ". $this->chat ." (id, login, text) values (NULL, '$userlogin', 'Запрещенный символ!')");
+		$this->send=$link->query("INSERT INTO ". $this->chat ." (id, login, text) values (NULL, 'Чат', 'Запрещенный символ!')");
 
 		} 
 		else 
 		{  
 		
+	
+	    $this->login=addslashes($userlogin);
+	    $this->text=addslashes($gettext);
+	    $param_1=$this->login;
+	    $param_2=$this->text;
+	    
 		/*Запишем в чат */	
-		$this->send=$link->query("INSERT INTO ". $this->chat ." (id, login, text) values (NULL, '$userlogin', '$gettext')") or exit ('Не получилось...');
+		$this->send=$link->query("INSERT INTO ". $this->chat ." (id, login, text) values (NULL,  '$param_1', '$param_2')") or exit (' :( ');
 	
 	    	  }
 		    }
-		 return $this->send;   
+	 return $this->send;   
 		 }
 		 
 
 		 
 }
 //Коннект к базе
-$result = new Connect();
-$result->mysql();
-$result->link->set_charset("utf8");
+$connection = new Connect();
+$connection->mysql();
+
 
 /*Отправим сообщения*/
 $send_messages = new Send_messages();
-$send_messages->CheckTextInput($result->link, $gettext, $userlogin);
+$send_messages->CheckTextInput($connection->link, $gettext, $userlogin);
 /*Удалим лишние*/
 
 require_once 'delete_messages.php';

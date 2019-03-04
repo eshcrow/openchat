0b234c9765d6d9b3ea42a80681b3a435fa26cd4d
dcc0 <ivangavr777@gmail.com>
2019-03-04 21:47:23 +0300

    Update chat.php

--- lib/view/chat.php
+++ lib/view/chat.php
@@ -1,35 +1,40 @@
 <?php
 //Дополнительно закрываем сраницу
 require_once $_SERVER['CONTEXT_DOCUMENT_ROOT'].'/conf/config.php';
+
 class printing {
-
+    
     protected $fetch;
-
-
+	
+	
 	 /*Запрашиваем последние 15 сообщений*/
-	    function fetching_out($link) {
-
+	    function fetchingOut($link) { 
+	 
 	 $this->fetch = $link->query("SELECT * from chat ORDER by id ASC LIMIT 15");
 	 return $this->fetch;
-
-
+	 
+	        
 	    }
+	 
+	 
+	 	    function printingOut($data) { 
 
-
-	 	    function printing_out($data) {
 	 	//*Выводим в цикле*/
-	 	while($val = mysqli_fetch_array($data))
+	 	while($val = mysqli_fetch_array($data)) 
 		echo '<strong>' . $val['login'] . ' </strong> ' . $val['text'] . '<br/>';
+
 	 }
-
-}
-
+	 
+}	
+   
     //Коннект к базе
-    $result = new connect();
+    $result = new Connect();
     $result->mysql();
     $result->link->set_charset("utf8");
     //Вывод
     $data = new printing();
-    $data-> printing_out($data->fetching_out($result->link));
+    $data-> printingOut($data->fetchingOut($result->link));
 
+
+    
 ?>

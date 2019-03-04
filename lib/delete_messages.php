dc316659178377718086d233fb9f3f156c5ec846
dcc0 <ivangavr777@gmail.com>
2019-03-04 15:11:22 +0300

    Update delete_messages.php

--- lib/delete_messages.php
+++ lib/delete_messages.php
@@ -3,11 +3,12 @@
     class count_delete_messages {
 	    
 	    protected $del;
+	    protected $chat='chat';
 	   
 	/*Количество всех сообщений*/
 	    function count_messages ($link) {
 
-	    return mysqli_fetch_array(mysqli_query($link,  "SELECT COUNT(*) FROM chat"));
+	    return mysqli_fetch_array(mysqli_query($link,  "SELECT COUNT(*) FROM ". $this->chat .""));
 
 		
 	    }
@@ -15,19 +16,21 @@
 		/*Удалим лишние*/
 		
 	function delete_messages ($link, $count_all_messages) {
+	
 		if ($count_all_messages > 15 )
 		{ 
-    	$this->del=mysqli_query($link, "DELETE FROM chat ORDER BY id ASC LIMIT 1  ") or exit ('not ok');
+    	$this->del=mysqli_query($link, "DELETE FROM ". $this->chat ."	 ORDER BY id ASC LIMIT 1  ") or exit ('not ok');
         return $this->del;
 		}
-		 
 	}
 	
 }	
+
+
 $count_delete_object = new	count_delete_messages();
 //запрашиваем все сообщения
 $count_all_messages=$count_delete_object->count_messages($result->link);
-//удалим
+
 $count_delete_object->delete_messages($result->link, $count_all_messages[0]);
 
 

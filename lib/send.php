d59b0a8743da2c2e0d4519bcc9e366acedb71580
dcc0 <ivangavr777@gmail.com>
2019-03-04 14:00:48 +0300

    Update send.php

--- lib/send.php
+++ lib/send.php
@@ -1,10 +1,18 @@
 <?php
 require_once $_SERVER['CONTEXT_DOCUMENT_ROOT'].'/conf/config.php';
 
+
+        /*Текст и имя из формы*/
 		$gettext=strip_tags($_GET['text']);
 		$userlogin=$_GET['userlogin'];
+		
+		
+class send_messages {    		
 
+    protected $send;
+    
 	function check_text_input($link, $gettext, $userlogin) {
+		
 		/*Проверим ввод текста*/
 		if(isset($gettext) && $gettext !='' ) 
 		{	
@@ -19,15 +27,19 @@ require_once $_SERVER['CONTEXT_DOCUMENT_ROOT'].'/conf/config.php';
 		{  
 		
 		/*Запишем в чат */	
-		mysqli_query($link, "INSERT INTO chat (id, login, text) values (NULL, '$userlogin', '$gettext')");
+		$this->send=mysqli_query($link, "INSERT INTO chat (id, login, text) values (NULL, '$userlogin', '$gettext')") or exit ('Не получилось...');
 	
+	    	  }
+		    }
+		 return $this->send;   
 		 }
-		 }
-
-		 }
-$link=$result->link;		
-		check_text_input($link, $gettext, $userlogin);
+		 
+}
+/*Отправим сообщения*/
+$send_messages = new send_messages();
+$send_messages->check_text_input($result->link, $gettext, $userlogin);
+/*Удалим лишние*/
 require_once 'delete_messages.php';
-		delete_messages ($link, $count_all_messages);	
+	
 		
 ?>

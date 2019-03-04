498079d19473dc290643e923aaa270c300f7e8f0
dcc0 <ivangavr777@gmail.com>
2019-03-04 15:07:07 +0300

    Update send.php

--- lib/send.php
+++ lib/send.php
@@ -9,38 +9,45 @@ require_once $_SERVER['CONTEXT_DOCUMENT_ROOT'].'/conf/config.php';
 		
 class send_messages {    		
 
+
     protected $send;
+    protected $chat="chat";
+
     
 	function check_text_input($link, $gettext, $userlogin) {
 		
 		/*Проверим ввод текста*/
 		if(isset($gettext) && $gettext !='' ) 
 		{	
-		
-		$gettext = substr($gettext, 0,300);
+			$gettext = substr($gettext, 0,300);
 		
 		if(!preg_match('/^[a-zа-яё0-9\.\,\!\?\;\#\&\/\)\(\-\+\=:\s]+$/ui', $gettext)) 
-		{
-			
-		$this->send=mysqli_query($link, "INSERT INTO chat (id, login, text) values (NULL, '$userlogin', 'Запрещенный символ!')");
+		{		
+		$this->send=mysqli_query($link, "INSERT INTO ". $this->chat ." (id, login, text) values (NULL, '$userlogin', 'Запрещенный символ!')");
 
 		} 
 		else 
 		{  
 		
 		/*Запишем в чат */	
-		$this->send=mysqli_query($link, "INSERT INTO chat (id, login, text) values (NULL, '$userlogin', '$gettext')") or exit ('Не получилось...');
+		$this->send=mysqli_query($link, "INSERT INTO ". $this->chat ." (id, login, text) values (NULL, '$userlogin', '$gettext')") or exit ('Не получилось...');
 	
-	    	  	}
-		    }
-		 return $this->send;   
-		 }
+	    	}
+		   
+		}
+		
+		return $this->send;   
+		
+		}
+		 
+
 		 
 }
 /*Отправим сообщения*/
 $send_messages = new send_messages();
 $send_messages->check_text_input($result->link, $gettext, $userlogin);
 /*Удалим лишние*/
+
 require_once 'delete_messages.php';
 	
 		

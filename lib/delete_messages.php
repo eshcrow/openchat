7f013b3f66f166ef7b4696a1f4a29c1f9ae3c6ce
dcc0 <ivangavr777@gmail.com>
2019-03-04 11:33:28 +0300

    Update delete_messages.php

--- lib/delete_messages.php
+++ lib/delete_messages.php
@@ -1,13 +1,13 @@
 <?php
 
 	/*Количество всех сообщений*/
-		$query = mysqli_query($link, "SELECT COUNT(*) FROM chat");
-		$row = mysqli_fetch_array($query);
-		
 
-	/*Удалим лишние*/
+		$query = mysqli_query($result->link,  "SELECT COUNT(*) FROM chat");
+		$row = mysqli_fetch_array($query);
 		$count_all_messages=$row['0'];
-	
+		
+		/*Удалим лишние*/
+		
 	function delete_messages ($link, $count_all_messages) {
 		if ($count_all_messages > 15 )
 		{ 
@@ -17,4 +17,5 @@
 		}
 		 
 	}
+	
 ?>

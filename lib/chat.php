304d577636e05cb49af6682d41e7dacd70d10cc3
dcc0 <ivangavr777@gmail.com>
2019-03-04 11:18:50 +0300

    Update chat.php

--- lib/chat.php
+++ lib/chat.php
@@ -1,16 +1,21 @@
 <?php
 require_once $_SERVER['CONTEXT_DOCUMENT_ROOT'].'/conf/config.php';
-	
-	function printing_out($link) {
-	    
-	 /*Запрашиваем последние 15 сообщений*/
-	$chat=mysqli_query($link, "SELECT * FROM chat  ORDER BY id ASC LIMIT 15");
-	
-	/*Выводим в цикле*/
-	 	while($val = mysqli_fetch_array($chat)) 
-		echo '<strong>' . $val['login'] . ' </strong> ' . $val['text'] . '<br/>';
-		
 
-	}
-		printing_out($link);
+class printing {
+        
+	 /*Запрашиваем последние 15 сообщений*/
+	    function printing_out($link) { 
+	 
+	 return $link->query("SELECT * from chat ORDER by id desc LIMIT 15");
+
+	 }
+	 
+}	
+   
+    $data = new printing();
+    $data=$data->printing_out($result->link);
+
+	//*Выводим в цикле*/
+	 	while($val = mysqli_fetch_array($data)) 
+		echo '<strong>' . $val['login'] . ' </strong> ' . $val['text'] . '<br/>';
 ?>
